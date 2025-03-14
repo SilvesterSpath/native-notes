a3499c60b2ea154cbedce618863fdfb872bac7af
SilvesterSpath <spath.sz@gmail.com>
2025-03-14 07:27:44 +0100

    button

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,21 @@
-import { Text, View, StyleSheet, Image } from 'react-native';
+import { Text, View, StyleSheet, Image, TouchableOpacity } from 'react-native';
 import PostImage from '../assets/images/post-it.png';
+import { useRouter } from 'expo-router';
 
 const HomeScreen = () => {
   return (
     <View style={styles.container}>
       <Image source={PostImage} style={{ width: 200, height: 200 }} />
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text style={styles.title}>Welcome to notes app</Text>
+      <Text style={styles.subtitle}>
+        Capture your thoughts any time anywhere
+      </Text>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => useRouter().push('notes')}
+      >
+        <Text style={styles.buttonText}>Get Started</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -23,6 +33,30 @@ const styles = StyleSheet.create({
     height: 100,
     marginBottom: 20,
     borderRadius: 10,
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    color: '#333',
+  },
+  subtitle: {
+    fontSize: 16,
+    color: '#666',
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: '#007bff',
+    paddingVertical: 12,
+    paddingHorizontal: 25,
+    borderRadius: 8,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
   },
 });
 
