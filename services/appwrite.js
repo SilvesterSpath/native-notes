8676215f91a4be9385793d7b0880a5447cda2f72
SilvesterSpath <spath.sz@gmail.com>
2025-03-21 10:01:11 +0100

    databaseService

--- services/appwrite.js
+++ services/appwrite.js
@@ -1,4 +1,5 @@
 import { Client, Databases } from 'react-native-appwrite';
+import { Platform } from 'react-native';
 
 const config = {
   endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
@@ -12,6 +13,17 @@ const config = {
 const client = new Client()
   .setEndpoint(config.endpoint)
   .setProject(config.projectId);
+
+switch (Platform.OS) {
+  case 'android':
+    client.setPlatform(process.env.EXPO_PUBLIC_APPWRITE_PACKAGE_NAME);
+    break;
+  case 'ios':
+    client.setPlatform(process.env.EXPO_PUBLIC_APPWRITE_BUNDLE_ID);
+    break;
+  default:
+    break;
+}
 
 const database = new Databases(client);
 
